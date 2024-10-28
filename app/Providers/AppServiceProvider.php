b58ff13556ca9e640cf2c5857167853a907eb1a8
Krishna <krishnakumar.webmobril@gmail.com>
2024-10-28 19:10:32 +0530

    add limit in boot

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+           // Add the following line
+    Schema::defaultStringLength(191);
     }
 }
