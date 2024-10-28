6c59d7b4d269434d5c5331acdb43819907dd7ffa
Krishna <krishnakumar.webmobril@gmail.com>
2024-10-28 19:18:31 +0530

    update

--- database/migrations/2021_11_16_114009_create_media_table.php
+++ database/migrations/2021_11_16_114009_create_media_table.php
@@ -10,23 +10,23 @@ class CreateMediaTable extends Migration
     {
         Schema::create('media', function (Blueprint $table) {
             $table->bigIncrements('id');
-
-            $table->morphs('model');
-            $table->uuid('uuid')->nullable()->unique();
-            $table->string('collection_name',100);
-            $table->string('name',100);
-            $table->string('file_name',100);
-            $table->string('mime_type',100)->nullable();
-            $table->string('disk',100);
-            $table->string('conversions_disk',100)->nullable();
+            $table->string('model_type', 191);
+            $table->unsignedBigInteger('model_id');
+            $table->char('uuid', 36)->nullable();
+            $table->string('collection_name', 100);
+            $table->string('name', 100);
+            $table->string('file_name', 100);
+            $table->string('mime_type', 100)->nullable();
+            $table->string('disk', 100);
+            $table->string('conversions_disk', 100)->nullable();
             $table->unsignedBigInteger('size');
-            $table->json('manipulations');
-            $table->json('custom_properties');
-            $table->json('generated_conversions');
-            $table->json('responsive_images');
+            $table->text('manipulations'); // Changed from json to text
+            $table->text('custom_properties'); // Changed from json to text
+            $table->text('generated_conversions'); // Changed from json to text
+            $table->text('responsive_images'); // Changed from json to text
             $table->unsignedInteger('order_column')->nullable();
-
-            $table->nullableTimestamps();
+            $table->timestamps();
         });
+        
     }
 }
